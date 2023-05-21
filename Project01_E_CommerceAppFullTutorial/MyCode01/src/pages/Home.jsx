34ea4566ed8f013dc01b030a45aa77ccd60cc39c
LeLam-vn <tvlelam@gmail.com>
2023-05-22 01:09:11 +0700

    220523-video-27:01

---
+++ Project01_E_CommerceAppFullTutorial/MyCode01/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar.jsx";
+import Announcement from "../components/Announcement.jsx";
+
+
+const Home = () => {
+    return (
+        <>
+            <Announcement/>
+            <Navbar/>
+        </>
+    )
+}
+export default Home
