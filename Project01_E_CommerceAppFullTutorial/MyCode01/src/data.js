69f6fb250526f2a8d1030cf7f8b09881d3c70b4e
LeLam-vn <tvlelam@gmail.com>
2023-05-22 16:44:36 +0700

    220523-video-50:54

---
+++ Project01_E_CommerceAppFullTutorial/MyCode01/src/data.js
@@ -0,0 +1,51 @@
+export const sliderItems =[
+    {
+        id: 1,
+        img:'https://i.ibb.co/SBWbpTD/FB-IMG-1603159754107.jpg',
+        title:'SUMMER SALE',
+        Desc: 'DO NOT COMPROMISE ON STYLE!GET FLAT 30 % OFF FOR NEW ARRIVALS',
+        bg: 'f5fafd'
+    },
+    {
+        id: 2,
+        img:'https://i.ibb.co/xXQdwHy/20190613-151537478-i-OS.jpg',
+        title:'WINTER SALE',
+        Desc: 'DO NOT COMPROMISE ON STYLE!GET FLAT 30 % OFF FOR NEW ARRIVALS',
+        bg: 'f5fafd'
+    },
+    {
+        id: 3,
+        img:'https://i.ibb.co/dDp32mz/20190613-151544438-i-OS.jpg',
+        title:'POPULAR SALE',
+        Desc: 'DO NOT COMPROMISE ON STYLE!GET FLAT 30 % OFF FOR NEW ARRIVALS',
+        bg: 'f5fafd'
+    },
+    {
+        id: 4,
+        img:'https://i.ibb.co/qyF22yX/FB-IMG-1599126852768.jpg',
+        title:'POPULAR SALE',
+        Desc: 'DO NOT COMPROMISE ON STYLE!GET FLAT 30 % OFF FOR NEW ARRIVALS',
+        bg: 'f5fafd'
+    },
+    {
+        id: 5,
+        img:'https://i.ibb.co/Vvdvh6g/FB-IMG-1601508340363.jpg',
+        title:'POPULAR SALE',
+        Desc: 'DO NOT COMPROMISE ON STYLE!GET FLAT 30 % OFF FOR NEW ARRIVALS',
+        bg: 'f5fafd'
+    },
+    {
+        id: 6,
+        img:'https://i.ibb.co/bRTQb2k/FB-IMG-1599783391471.jpg',
+        title:'POPULAR SALE',
+        Desc: 'DO NOT COMPROMISE ON STYLE!GET FLAT 30 % OFF FOR NEW ARRIVALS',
+        bg: 'f5fafd'
+    },
+    {
+        id: 7,
+        img:'https://i.ibb.co/bvpMPSj/FB-IMG-1600046244460-2.jpg',
+        title:'POPULAR SALE',
+        Desc: 'DO NOT COMPROMISE ON STYLE!GET FLAT 30 % OFF FOR NEW ARRIVALS',
+        bg: 'f5fafd'
+    }
+]
